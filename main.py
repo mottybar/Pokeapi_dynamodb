90c19f7a4ea668b475e77f95d43797a2b1e4346b
mottybar <mottybar20@gmail.com>
2023-10-05 00:26:49 +0300

    update

--- main.py
+++ main.py
@@ -2,44 +2,103 @@ import os
 import boto3
 import requests
 from dotenv import load_dotenv
+import pprint as pp
+import random as rd
+
+
+def create_dynamodb_table():
+    # Get the service resource.
+    dynamodb = boto3.resource('dynamodb', region_name=os.getenv("AWS_REGION"))
+
+    # Create the DynamoDB table.
+    table = dynamodb.create_table(
+        TableName='Pokemons',
+        KeySchema=[
+            {
+                'AttributeName': 'name',
+                'KeyType': 'HASH'
+            },
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'name',
+                'AttributeType': 'S'
+            },
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 5,
+            'WriteCapacityUnits': 5
+        }
+    )
+
+    # Wait until the table exists.
+    table.wait_until_exists()
+
+    # Print out some data about the table.
+    print(table.item_count)
+    return table
+
+
+def download_pokemons(api_url="https://pokeapi.co/api/v2/pokemon"):
+    response = requests.get(api_url)
+    result = response.json()
+    return result
+
+
+def insert_item_to_table(details, table):
+    table.put_item(
+        Item={
+            'name': details["name"],
+            'id': details["id"],
+            'weight': details["weight"],
+            'height': details["height"]
+        }
+    )
+
+
+def check_if_item_exist(item, table):
+    response = table.get_item(
+        Key={
+            'name': item
+        }
+    )
+    if 'Item' in response:
+        return True
+    else:
+        return False
+
+
+def get_an_item(item, table):
+    response = table.get_item(
+        Key={
+            'name': item,
+        }
+    )
+    get_pokemon = response['Item']
+    for (k, v) in get_pokemon.items():
+        print(k, "=", v)
 
 
 load_dotenv()
-
-# Get the service resource.
-dynamodb = boto3.resource('dynamodb',region_name=os.getenv("AWS_REGION"))
-
-# Create the DynamoDB table.
-table = dynamodb.create_table(
-    TableName='Pokemons',
-    KeySchema=[
-        {
-            'AttributeName': 'name',
-            'KeyType': 'HASH'
-        },
-        {
-            'AttributeName': 'id',
-            'KeyType': 'RANGE'
-        }
-    ],
-    AttributeDefinitions=[
-        {
-            'AttributeName': 'name',
-            'AttributeType': 'S'
-        },
-        {
-            'AttributeName': 'id',
-            'AttributeType': 'N'
-        },
-    ],
-    ProvisionedThroughput={
-        'ReadCapacityUnits': 5,
-        'WriteCapacityUnits': 5
-    }
-)
-
-# Wait until the table exists.
-table.wait_until_exists()
-
-# Print out some data about the table.
-print(table.item_count)
+pokemons_table = create_dynamodb_table()
+continue_drawing = True
+while continue_drawing:
+    draw = input("would you like to draw a pokemon?['y' or 'n'] ").lower()
+    if draw == 'y':
+        res = download_pokemons()
+        pokemons = res["results"]
+        pokemon = rd.choice(pokemons)
+        pokemon_name = pokemon["name"]
+        pokemon_url = pokemon["url"]
+        # print(pokemon_name)
+        # print(pokemon_url)
+        pokemon_details = download_pokemons(pokemon_url)
+        if not check_if_item_exist(pokemon_name, pokemons_table):
+            insert_item_to_table(pokemon_details, pokemons_table)
+        get_an_item(pokemon_name, pokemons_table)
+    elif draw == 'n':
+        print("no pokemon will be drawn")
+        continue_drawing = False
+    else:
+        print("invalid input. please enter 'y' or 'n' ")
+print("Goodbye!!!!")
